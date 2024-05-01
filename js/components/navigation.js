fa8879114d7a9d3ffee3b7f0775d15e251029232
isaul19 <saulhola348@gmail.com>
2024-05-01 18:41:36 -0500

    base project

---
+++ js/components/navigation.js
@@ -0,0 +1,52 @@
+export class Navigation extends HTMLElement {
+  static name = "wc-navigation";
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.#render();
+  }
+
+  #routes = [
+    {
+      href: "/",
+      text: "Inicio",
+    },
+    {
+      href: "#about",
+      text: "Sobre mi",
+    },
+    {
+      href: "#project",
+      text: "Proyectos",
+    },
+    {
+      href: "#contact",
+      text: "Contacto",
+    },
+  ];
+
+  #render() {
+    this.shadowRoot.innerHTML = /*html*/ `
+    <style>@import url("/css/components/navigation.css");</style>
+    <header class="${Navigation.name} container">
+      <nav class="nav">
+        <a class="logo h6" href="/">iSaul19</a>
+        <ul class="items">
+          ${this.#routes
+            .map(
+              (route) => /*html*/ `
+                <li class="item">
+                  <a class="link h6" href="${route.href}">${route.text}</a>
+                </li>
+                `
+            )
+            .join("")}
+        </ul>
+      </nav>
+    </header>
+    `;
+  }
+}
+
+customElements.define(Navigation.name, Navigation);
